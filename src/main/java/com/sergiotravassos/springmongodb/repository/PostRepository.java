a534b6581a2152f392c91cd473e6c02f40063ce8
sergiotravassos <sergiotravassos@outlook.com>
2022-07-26 17:20:03 +0100

    consulta simples com @Query

--- src/main/java/com/sergiotravassos/springmongodb/repository/PostRepository.java
+++ src/main/java/com/sergiotravassos/springmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.sergiotravassos.springmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sergiotravassos.springmongodb.domain.Post;
@@ -12,4 +13,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	// Query Methods
 	List<Post> findByTitleContaining(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 }
