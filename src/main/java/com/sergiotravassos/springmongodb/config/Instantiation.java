8c72e35989f613f10c3d0832cbd5f97a1eb49aef
sergiotravassos <sergiotravassos@outlook.com>
2022-07-26 17:49:32 +0100

    Consulta com varios criterios

--- src/main/java/com/sergiotravassos/springmongodb/config/Instantiation.java
+++ src/main/java/com/sergiotravassos/springmongodb/config/Instantiation.java
@@ -39,12 +39,12 @@ public class Instantiation implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2022"), "Teste Title 1", "Teste mensagem body 1", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("21/03/2022"), "Teste Title 2", "Teste mensagem body 2", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("20/03/2022"), "Teste Title 1", "Teste mensagem body 1", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2022"), "Teste Title 2", "Teste mensagem body 2", new AuthorDTO(maria));
 		
 		CommentDTO c1 = new CommentDTO("Teste comentario 1 no post 1", sdf.parse("21/03/2022"), new AuthorDTO(alex));
-		CommentDTO c2 = new CommentDTO("Teste comentario 2 no post 1", sdf.parse("21/03/2022"), new AuthorDTO(bob));
-		CommentDTO c3 = new CommentDTO("Teste comentario 3 em post 2", sdf.parse("21/03/2022"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Teste comentario 2 no post 1", sdf.parse("22/03/2022"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Teste comentario 3 em post 2", sdf.parse("23/03/2022"), new AuthorDTO(alex));
 		
 		post1.getComments().addAll(Arrays.asList(c1, c2));
 		post2.getComments().addAll(Arrays.asList(c3));
