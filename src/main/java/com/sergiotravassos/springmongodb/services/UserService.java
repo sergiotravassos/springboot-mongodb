c65497fbbf7057ca2a667e589237fe90b081980d
sergiotravassos <sergiotravassos@outlook.com>
2022-07-26 12:36:52 +0100

    Insercao de usuario com POST

--- src/main/java/com/sergiotravassos/springmongodb/services/UserService.java
+++ src/main/java/com/sergiotravassos/springmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sergiotravassos.springmongodb.domain.User;
+import com.sergiotravassos.springmongodb.dto.UserDTO;
 import com.sergiotravassos.springmongodb.repository.UserRepository;
 import com.sergiotravassos.springmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
