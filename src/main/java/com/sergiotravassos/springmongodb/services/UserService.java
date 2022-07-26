99777ad8b9b3ee251ccb8c8bdef442361e64c655
sergiotravassos <sergiotravassos@outlook.com>
2022-07-26 12:22:09 +0100

    obtendo um usuario por id

--- src/main/java/com/sergiotravassos/springmongodb/services/UserService.java
+++ src/main/java/com/sergiotravassos/springmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.sergiotravassos.springmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sergiotravassos.springmongodb.domain.User;
 import com.sergiotravassos.springmongodb.repository.UserRepository;
+import com.sergiotravassos.springmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,8 @@ public class UserService {
 		return userRepository.findAll();
 	}
 	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
 }
